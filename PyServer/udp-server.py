efe1f022ec779f6f4633c9f7d50155d8d24481d8
Zhengmao <malker@qq.com>
2021-02-18 13:14:27 -0800

    尝试把接受的字符串变成指令

--- PyServer/udp-server.py
+++ PyServer/udp-server.py
@@ -43,11 +43,13 @@ print('Bind UDP on 9999...')
 
 while True:
     # 接收数据:
-    data, addr = s.recvfrom(1024)
+    str_command, addr = s.recvfrom(1024)
     x = datetime.datetime.now()
     print('Received from %s:%s.' % addr)
-    print(data.decode('utf-8'))
+    str_command = str_command.decode('utf-8')
+    print(str_command)
     # reply = str(x)
     # reply += '\nHello, %s!' % data.decode('utf-8')
-    reply = str(x) + '\n' + Room_10.getroominfo # + listrooms(rooms)
+    # if str_command = 
+    reply = str(x) + '\n' + Room_10.getroominfo() # + listrooms(rooms)
     s.sendto(reply.encode('utf-8'), addr)
