c4e7bf2c35dfc69a6d94287e09df7512293e5494
Zhengmao <malker@qq.com>
2021-02-20 10:51:10 -0800

    正在优化对于roomS和userS的操作

---
+++ Class/list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 这是生成房间的功能
+import Room
+def creatlotsroom(arr_room,num):
+    for i in range(num):
+        arr_room.append(Room.creatchatroom('Room-'+str(i),i,'Test Uesr'))
+# 这个是显示房间信息的功能
+def listrooms(arr_room):
+    str_roomlist = ''
+    for i in range(len(arr_room)):
+        str_roomlist += str(arr_room[i].name+'\n')
+    return str_roomlist
+rooms = []
+creatlotsroom(rooms,20)
+print(listrooms(rooms))
