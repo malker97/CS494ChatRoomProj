b1eb611d2f58ac7cb244853176fedf1be5d80ea2
Zhengmao <malker@qq.com>
2021-03-01 14:48:13 -0800

    客户端换成了Python，服务器端开始做指令的匹配

--- PyServer/udp-server.py
+++ PyServer/udp-server.py
@@ -5,17 +5,8 @@ import User
 import Room
 import List
 
-
-# 构造测试房间组的方法
-# rooms = []
-# List.creatlotsroom(rooms,40)
-#
-# #测试用的字符串
-# str_getroomhis = 'GET ROOM HIS'
-# str_getroomlist = 'GET ROOM LIST'
-# str_getroominfo = 'GET ROOM INFO'
-#生成了一个UDP服务器名字叫S
-
+rooms = []
+List.creatlotsroom(rooms,40)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -29,12 +20,11 @@ while True:
     str_command, addr = s.recvfrom(1024)
     x = datetime.datetime.now()
     print('Received from %s:%s.' % addr)
-    # print(addr)
     str_command = str_command.decode('utf-8')
     print(str_command)
     str_ans = ''
-    #测试部分
-    # str_ans = List.listrooms(rooms)
-    #
+    if str_command == 'Get Room Info':
+        str_ans = List.listrooms(rooms)
+  
     reply = str(x) + '\n'  + str_ans # + listrooms(rooms)
     s.sendto(reply.encode('utf-8'), addr)
