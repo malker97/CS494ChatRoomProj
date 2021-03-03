8ceb55a586c59b423cfd73871d81509604ad438e
Zhengmao <malker@qq.com>
2021-03-03 13:13:43 -0800

    更新了字符串匹配，但是不确定能不能成功

--- PyServer/udp-server.py
+++ PyServer/udp-server.py
@@ -7,8 +7,26 @@ import List
 
 rooms = []
 List.creatlotsroom(rooms,40)
-# def switchcases(s: str)->str:
-
+def switchcases(s: str)->str:
+    # b'Server status', # Client can connect to a server
+    # b'Creat new room', # Client can create a room 
+    # b'Get Room Info', # Client can list members of a room
+    # b'Get Room List', # Client can list all rooms
+    # b'Get Room Chat History', 
+    # b'Send Message ~20~ This is a Test Message frome Client'# Client can send messages to a room
+    # b'Disconnect!' # Client can disconnect from a server
+    # b'Server plz shut down' # Server can disconnect from clients
+    # # Client can gracefully handle server crashes
+    # # Server can gracefully handle client crashes
+    ans_str = ''
+    if s == 'Server status':
+        ans_str = "Connected Ok"
+    elif s == 'Creat new room':
+        rooms.append(Room.creatchatroom('Room-'+str(len(rooms)),len(rooms),'Test Uesr'))
+        ans_str = 'Room has created'
+    # elif s == 'Get Room Info':
+    #     for i in range(len(rooms))
+    return ans_str
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -26,7 +44,7 @@ while True:
     print(str_command)
     if str_command == 'Server plz shut down':
         print('Server get the shutdown command, it will shutdown now!')
-        s.sendto('Server get the shutdown command, it will shutdown now!'.encode('utf-8'),addr)
+        # s.sendto('Server get the shutdown command, it will shutdown now!'.encode('utf-8'),addr)
         break
     str_ans = ''
     if str_command == 'Get Room List':
